3d181fb5263ab7fb2083040d77ad2d8066b7c4de
Abner Eliezer Betancourt <abner.betancourt@agilesolutions.com>
2024-12-05 15:15:43 -0600

    feat: :sparkles: Creating Auth page

---
+++ frontend/src/pages/Authentication/index.tsx
@@ -0,0 +1,11 @@
+import NavOnlyLayout from '@/layouts/NavOnlyLayout';
+
+const Authentication: React.FC = () => {
+	return (
+		<NavOnlyLayout>
+			<h1>Authentication</h1>
+		</NavOnlyLayout>
+	);
+};
+
+export default Authentication;
