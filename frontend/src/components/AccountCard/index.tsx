b49fc9edf6ca4c9ae923a125bdbfb3bda7381646
Abner Eliezer Betancourt <abner.betancourt@agilesolutions.com>
2024-12-11 09:49:27 -0600

    fix: :adhesive_bandage: adding support to icons and colors

--- frontend/src/components/AccountCard/index.tsx
+++ frontend/src/components/AccountCard/index.tsx
@@ -1,28 +1,59 @@
-import { Wallet } from 'lucide-react';
+import { CreditCard, Landmark, Pencil, PiggyBank, Smartphone, Trash, Wallet } from 'lucide-react';
+
+import { useDeleteAccount } from '@/hooks/services/useAccounts';
 
 interface AccountCardProps {
 	balance?: number;
 	currency?: string;
 	name?: string;
-	backgroundColor?: string;
+	color?: string;
+	icon?: string;
 }
+
+const getIcon = (icon: string) => {
+	switch (icon) {
+		case 'Wallet':
+			return <Wallet />;
+		case 'CreditCard':
+		case 'DebitCard':
+			return <CreditCard />;
+		case 'Savings':
+			return <PiggyBank />;
+		case 'Bank':
+			return <Landmark />;
+		case 'ElectronicWallet':
+			return <Smartphone />;
+		default:
+			return <Wallet />;
+	}
+};
 
 const AccountCard: React.FC<AccountCardProps> = ({
 	balance = 0,
 	currency = 'USD',
 	name = 'My Account',
-	backgroundColor = '#fff',
+	color: backgroundColor = '#fff',
+	icon = 'Wallet',
 }) => {
+	const deleteAccount = useDeleteAccount();
+
 	return (
 		<div className="card shadow-md w-100 h-32" style={{ backgroundColor }}>
 			<div className="card-body">
 				<div className="card-title">
-					<Wallet />
+					{getIcon(icon)}
 					{name}
 				</div>
 				<p className="text-left">
 					{currency} <span className="text-xl font-bold">{balance}</span>
 				</p>
+				{/* update and delete buttons as icons*/}
+				<button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 text-error">
+					<Trash size={16} />
+				</button>
+				<button className="btn btn-sm btn-circle btn-ghost absolute right-10 top-2">
+					<Pencil size={16} />
+				</button>
 			</div>
 		</div>
 	);
